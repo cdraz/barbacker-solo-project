a3921267d051dff22adb9f1d476701a74d048e62
Christopher Razidlo <cd.razidlo@gmail.com>
2022-02-07 14:12:53 -0600

    ingredient router now returns all ingredients from cocktaildb and store in saga

--- server/routes/ingredient.router.js
+++ server/routes/ingredient.router.js
@@ -10,7 +10,16 @@ const axios = require('axios');
 router.get('/', (req, res) => {
   // GET all ingredients from the third party api
   console.log('in /api/ingredient');
-  // axios.get(`www.thecocktaildb.com/api/json/v2/${process.env.COCKTAIL_API_KEY}/list.php?i=list`)
+  // console.log('api key is', process.env.COCKTAIL_API_KEY);
+  axios.get(`https://www.thecocktaildb.com/api/json/v2/${process.env.COCKTAIL_API_KEY}/list.php?i=list`)
+  .then( apiRes => {
+    console.log('api response received in get /ingredient');
+    res.send(apiRes.data);
+  })
+  .catch( err => {
+    console.error('error in get /ingredient', err);
+    res.sendStatus(500);
+  });
 
 });
 
