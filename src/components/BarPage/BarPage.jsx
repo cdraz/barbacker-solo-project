63ace232230445b75a8e6d9f9209d9ac2cc6f588
Christopher Razidlo <cd.razidlo@gmail.com>
2022-02-07 13:55:22 -0600

    bar view now dispatches to cocktaildb saga and reaches ingredient router

--- src/components/BarPage/BarPage.jsx
+++ src/components/BarPage/BarPage.jsx
@@ -5,12 +5,19 @@ import { useSelector, useDispatch } from 'react-redux';
 function BarPage() {
 
     // Store access, dispatch hook
-    const store = useSelector(store => store);
+    const ingredients = useSelector(store => store.ingredientsReducer);
+    const dispatch = useDispatch();
 
+    // Declare onGetIngredients
+    const onGetIngredients = () => {
+        dispatch({
+            type: 'GET_INGREDIENTS'
+        });
+    }
     return (
         <div>
         <h3>Bar Page</h3>
-        <button>Get ingredients</button>
+        <button onClick={onGetIngredients}>Get ingredients</button>
         <select>
             
         </select>
