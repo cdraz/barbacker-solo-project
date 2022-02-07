3b67b0e5024de8fc71b1300d0002aeb2b572a59b
Christopher Razidlo <cd.razidlo@gmail.com>
2022-02-07 11:41:42 -0600

    table creation added to database.sql

--- database.sql
+++ database.sql
@@ -1,4 +1,4 @@
-
+-- Create database "barbacker"
 -- USER is a reserved keyword with Postgres
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
@@ -7,4 +7,33 @@ CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
+);
+
+CREATE TABLE "bar_ingredients" (
+    "id" SERIAL PRIMARY KEY,
+    "userId" INT NOT NULL,
+    "apiID" INT NOT NULL,
+    "apiString" VARCHAR(255) NOT NULL;
+);
+
+CREATE TABLE "saved_api_recipes" (
+    "id" SERIAL PRIMARY KEY,
+    "apiId" INTEGER NOT NULL,
+    "userId" INTEGER NOT NULL REFERENCES "user"
+);
+
+CREATE TABLE "user_recipes" (
+    "id" SERIAL PRIMARY KEY,
+    "userId" INTEGER NOT NULL REFERENCES "user",
+    "name" VARCHAR(255) NOT NULL,
+    "instructions" VARCHAR(2400) NOT NULL,
+    "image" VARCHAR(1200) NOT NULL,
+    "alcoholic" BOOLEAN NOT NULL
+);
+
+CREATE TABLE "user_recipes_ingredients" (
+    "id" SERIAL PRIMARY KEY,
+    "recipeId" INTEGER NOT NULL REFERENCES "user_recipes",
+    "apiIngredientName" VARCHAR(255) NOT NULL,
+    "apiIngredientId" INTEGER NOT NULL
 );
