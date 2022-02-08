26e65e2a441514eed68303bd87fc4b574c1b82e8
Christopher Razidlo <cd.razidlo@gmail.com>
2022-02-08 15:10:09 -0600

    get endpoint complete for bar view, data gets to client and renders

--- src/redux/sagas/bar.saga.js
+++ src/redux/sagas/bar.saga.js
@@ -1,18 +1,27 @@
 import { put, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
 
+function* getBarIngredients() {
+    try {
+        let response = yield axios.get('/api/bar');
+        yield put({
+            type: 'SET_BAR_INGREDIENTS',
+            payload: response.data
+        });
+    }
+    catch(err) {
+        console.error('Error with getBarIngredients', err);
+    }
+}
+
 function* postBarIngredients(action) {
     try {
         let response = yield axios.post('/api/bar', action.payload);
         // api returns an array of objects with the first property being the string of the ingredient
         // We want only an array of ingredient strings, so use .map() to return an array of just the strings    
-        // let payload = response.data.map( function(obj){
-        //     return obj.strIngredient1;
-        // })
-        // yield put({
-        //     type: 'SET_USER_INGREDIENTS',
-        //     payload: payload
-        // });
+        yield put({
+            type: 'GET_BAR_INGREDIENTS',
+        });
     }
     catch(err) {
         console.error('Error with postBarIngredients:', err);
@@ -21,6 +30,7 @@ function* postBarIngredients(action) {
 
 function* barSaga() {
     yield takeLatest('POST_BAR_INGREDIENTS', postBarIngredients);
+    yield takeLatest('GET_BAR_INGREDIENTS', getBarIngredients);
 }
 
 export default barSaga;
