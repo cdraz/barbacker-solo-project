f7b0747d5564046b58688b84b9fb8047e7571a9e
Christopher Razidlo <cd.razidlo@gmail.com>
2022-02-09 09:41:30 -0600

    nav icons added, links changed to new components

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -25,20 +25,26 @@ function Nav() {
         {user.id && (
           <>
             <Link className="navLink" to="/bar">
-              My Bar
+              <img src="https://cdn-icons-png.flaticon.com/512/3184/3184586.png" alt="bar-icon" style={{ height: 30, width: 30 }} />
             </Link>
 
-            <Link className="navLink" to="/info">
-              Info Page
+            <Link className="navLink" to="/search">
+              <img src="https://cdn-icons.flaticon.com/png/512/2652/premium/2652234.png?token=exp=1644421700~hmac=a7fad12441d1ace590a63dc4a40194fc" alt="search-icon" style={{ height: 30, width: 30 }} />
+            </Link>
+
+            <Link className="navLink" to="/discover">
+              <img src="https://cdn-icons.flaticon.com/png/512/566/premium/566359.png?token=exp=1644421625~hmac=a439ff45497066c0277e85cbfe7fb4cc" alt="idea-icon" style={{ height: 30, width: 30 }} />
+            </Link>
+
+            <Link className="navLink" to="/recipes">
+              <img src="https://cdn-icons.flaticon.com/png/512/3438/premium/3438070.png?token=exp=1644421926~hmac=5e7aaec51310284fa64da69ad339b478" alt="recipe-book-icon" style={{ height: 30, width: 30 }} />
             </Link>
 
             <LogOutButton className="navLink" />
           </>
         )}
 
-        <Link className="navLink" to="/about">
-          About
-        </Link>
+
       </div>
     </div>
   );
